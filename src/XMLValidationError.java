8171662364794c36171b05111b5249038a77e539
Damiano Monelli <monelli.damiano@gmail.com>
2011-07-12 16:54:39 +0200

    added source model reader

---
+++ src/XMLValidationError.java
@@ -0,0 +1,22 @@
+
+
+import org.dom4j.DocumentException;
+
+/**
+ * Trivial Exception subclass with additional information for
+ * error reporting.
+ */
+public class XMLValidationError extends RuntimeException {
+    private String fileName;
+
+    public XMLValidationError(String fileName, DocumentException cause) {
+        super(cause);
+
+        this.fileName = fileName;
+    }
+
+    /** The full path of the invalid file */
+    public String getFileName() {
+        return fileName;
+    }
+}
